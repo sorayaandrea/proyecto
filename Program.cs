b0beb775667fa4673665cb3f100ab87f0e598508
sorayaandrea <53837900+sorayaandrea@users.noreply.github.com>
2019-08-21 16:30:24 -0500

    variables

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ namespace proyecto
 {
     class Program
     {
+        EnvironmentVariableTarget } }
         static void Main(string[] args)
-        {
+        
+    var int =edad;
             Console.WriteLine("Hello World!");
             Console.Writeline digite el nombre
     }
