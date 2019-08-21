f026c3c90db9d44fc544929de2f67ee4f8c4cddf
sorayaandrea <53837900+sorayaandrea@users.noreply.github.com>
2019-08-21 16:23:47 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Writeline digite el nombre
+    }
+}
